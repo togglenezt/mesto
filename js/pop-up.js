8409928ae84b90d4b06d9502a7deb01d4b963dfd
Salomatov Nikita <togglenezt@yandex.ru>
2022-08-22 20:54:43 +0300

    ПР 5

--- js/pop-up.js
+++ js/pop-up.js
@@ -13,37 +13,148 @@ let jobInput =  formElement.querySelector('.form__field_type_job');
 // Находим поля Профиля в DOM
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
+const popUpContainer = document.querySelector('.pop-up__container');
+// Массив картинок, появляются при загрузке страницы
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+// Находим шаблон 
+const placeTemplate = document.querySelector('#place-template').content;
+//место куда будет вставляться шаблон
+const gallery = document.querySelector('.gallery');
+
+//Код для вставки шаблонов при загрузке страницы 
+initialCards.forEach(function(item){
+    //// клонируем содержимое тега template
+    const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+
+    placeElement.querySelector('.place__image').src = item.link;
+    placeElement.querySelector('.place__image').alt = item.name;
+    placeElement.querySelector('.place__title').textContent = item.name;
+
+    placeElement.querySelector('.place__like').addEventListener('click', function(evt){
+      evt.target.classList.toggle('place__like_click_active');
+  });
+
+    const deliteButton =  placeElement.querySelector('.place__delite-button');
+    deliteButton.addEventListener('click',function(){
+        const placeRemove = deliteButton.closest('.place');
+        placeRemove.remove();
+    });
+
+    gallery.append(placeElement);
+
+    placeElement.querySelector('.place__image').addEventListener('click', function(evt){
+      document.querySelector('.pop-up__image').src = evt.target.src;
+      document.querySelector('.pop-up__text-image').textContent = evt.target.alt;
+      popUp.classList.toggle('pop-up_opened');
+      popUp.querySelector('.pop-up__container').classList.add('pop-up__image-container_open');
+      popUp.querySelector('.pop-up__image-container').classList.add('pop-up__image-container_visible');
+    });
+
+});
 
 
+const addPlaceForm = document.querySelector('.form-add-place');
+const fieldPlaceName = addPlaceForm.querySelector('.form__field_type_place-name');
+const fieldPlaceLink = addPlaceForm.querySelector('.form__field_type_place-link');
+
+const addButtonPlace = document.querySelector('.profile__add-button');
+addButtonPlace.addEventListener('click', function(){
+    popUp.classList.toggle('pop-up_opened');
+    popUpContainer.querySelector('.form-add-place').classList.add('form_visible');
+    fieldPlaceName.value = '';
+    fieldPlaceLink.value = '';
+});
+
+
+function formAddPlace (evt){
+    evt.preventDefault();
+    //// клонируем содержимое тега template
+    const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+
+    placeElement.querySelector('.place__image').src = fieldPlaceLink.value;
+    placeElement.querySelector('.place__image').alt = fieldPlaceName.value;
+    placeElement.querySelector('.place__title').textContent = fieldPlaceName.value;
+
+    placeElement.querySelector('.place__like').addEventListener('click', function(evt){
+        evt.target.classList.toggle('place__like_click_active');
+    });
+
+    const deliteButton =  placeElement.querySelector('.place__delite-button');
+    deliteButton.addEventListener('click',function(){
+        const placeRemove = deliteButton.closest('.place');
+        placeRemove.remove();
+    });
+
+    gallery.prepend(placeElement);
+    popUp.classList.remove('pop-up_opened');
+
+    placeElement.querySelector('.place__image').addEventListener('click', function(evt){
+      document.querySelector('.pop-up__image').src = evt.target.src;
+      document.querySelector('.pop-up__text-image').textContent = evt.target.alt;
+      popUp.classList.toggle('pop-up_opened');
+      popUpContainer.querySelector('.form-add-place').classList.remove('form_visible');
+      popUp.querySelector('.pop-up__container').classList.add('pop-up__image-container_open');
+      popUp.querySelector('.pop-up__image-container').classList.add('pop-up__image-container_visible');
+    });
+
+}
+
+
+addPlaceForm.addEventListener('submit', formAddPlace);
+
+/*-------------------------------------- Спринт 4 ----------------------------------*/
 function popUpOpen() {
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;    
     popUp.classList.toggle('pop-up_opened');
+    popUpContainer.querySelector('.form').classList.add('form_visible');
+    popUpContainer.querySelector('.form-add-place').classList.remove('form_visible');
+
 }
 
 function popUpClose() {
     popUp.classList.toggle('pop-up_opened');
+    popUpContainer.querySelector('.form').classList.remove('form_visible');
+    popUpContainer.querySelector('.form-add-place').classList.remove('form_visible');
+    popUp.querySelector('.pop-up__container').classList.remove('pop-up__image-container_open');
+    popUp.querySelector('.pop-up__image-container').classList.remove('pop-up__image-container_visible');
 }
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+    evt.preventDefault(); 
 
-    
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    // Вставьте новые значения с помощью textContent
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     popUp.classList.remove('pop-up_opened');
+    popUpContainer.querySelector('.form').classList.remove('form_visible');
+    popUpContainer.querySelector('.form-add-place').classList.remove('form_visible');
 }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 
 formElement.addEventListener('submit', formSubmitHandler);
 buttonEdit.addEventListener('click', popUpOpen);
