9ef6585f6e5eff7c8348ccd58bcc02ddf729eae4
Salomatov Nikita <togglenezt@yandex.ru>
2022-10-16 16:16:47 +0300

    feat: ПР8 на ревью

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
