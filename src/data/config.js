6c3fd4f55a127194c45b5433ab20db60e90aef8a
Salomatov Nikita <togglenezt@yandex.ru>
2022-10-30 21:44:09 +0300

    fix: загрузка карточек

---
+++ src/data/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  formSelector: '.form',
+  inputSelector: '.form__field',
+  submitButtonSelector: '.form__submit-button',
+  popupErrorSelector: '.form__text-error',
+  inactiveButtonClass: 'form__submit-button_disabled',
+  inputErrorClass: 'form__field_type_error',
+  errorClass: 'form__field_type_active'
+};
